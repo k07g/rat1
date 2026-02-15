a566c26cd1625ae11c5e0caa62620c9eb922122e
k07g <korenaga.makoto@ea-sys.jp>
2026-02-15 11:09:03 +0900

    Add StateFormUC

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,13 @@ import './index.css'
 import App from './App.tsx'
 import SelectComp from './SelectComp.tsx'
 import StateForm from './StateForm.tsx'
+import StateFormUC from './StateFormUC.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
     <SelectComp />
     <StateForm />
+    <StateFormUC />
   </StrictMode>,
 )
